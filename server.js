ea3d74de5aa044b617bb38a7a08c046993dc1425
Norayr <norayr.babayann@gmail.com>
2026-02-17 20:21:30 +0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json()); // parse JSON bodies
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://job-portal-platform-frontend-ictj.onrender.com",
     credentials: true,
   })
 );
